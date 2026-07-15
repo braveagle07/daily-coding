1030101f334d8a061b50ac3a68ba052872183dc8
Vasanth  sesetti <vasanthsessetti@gmail.com>
2026-07-15 20:47:02 +0530

    Add Java sample program in java-samples subfolder

---
+++ java-samples/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programming!");
+
+        // Simple addition
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " = " + sum);
+
+        // Loop example
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
