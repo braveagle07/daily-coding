cd3a56e09df5f11fe49e69ae36ebe9cc72b08e62
Vasanth  sesetti <vasanthsessetti@gmail.com>
2026-07-22 21:45:39 +0530

    Add assignment operators example in Java

---
+++ assignmentoperators.java
@@ -0,0 +1,32 @@
+class vasanth {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 50000L;
+        float f = 10.5f;
+        double d = 99.99;
+        char c = 'A';
+        boolean flag = true;
+        String name = "Vasanth";
+
+        int a = 10;
+        a += 5;
+        a -= 2;
+        a *= 3;
+        a /= 4;
+        a %= 3;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+        System.out.println("String: " + name);
+
+        System.out.println("Assignment result: " + a);
+    }
+}
