b40357fe3a65b071fb42c0b8f3bda3ad0f09d6eb
Vasanth  sesetti <vasanthsessetti@gmail.com>
2026-08-04 21:18:23 +0530

    Implement dynamic programming solution for climbing stairs
    Added a dynamic programming approach to calculate the number of ways to climb stairs.

--- dp1.py
+++ dp1.py
@@ -7,3 +7,22 @@ def climb_stairs(n):
     return b
 n = 5
 print(climb_stairs(n))
+
+#2nd way
+def climb_stairs(n):
+    # dp[i] = number of ways to reach step i
+    if n <= 2:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[1] = 1  # 1 way: (1)
+    dp[2] = 2  # 2 ways: (1+1), (2)
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+n = 5
+print(climb_stairs(n))
